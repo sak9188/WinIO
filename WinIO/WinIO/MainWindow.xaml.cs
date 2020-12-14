de46dbb8d80cc604e3fec662999bb3d8fcd5870a
HK <sak9188@163.com>
2020-12-14 18:00:48 +0800

    添加额外的项目与实现

--- WinIO/WinIO/MainWindow.xaml.cs
+++ WinIO/WinIO/MainWindow.xaml.cs
@@ -20,9 +20,59 @@ namespace WinIO
     /// </summary>
     public partial class MainWindow
     {
+        private List<TabControl> tabs = new List<TabControl>();
+
+        private TabItem CreateTabItem(string name)
+        {
+            TextBox textBox = new TextBox();
+            textBox.Style = (Style)Resources["OuputTextBox"];
+            Grid grid = new Grid();
+            grid.Children.Add(textBox);
+            TabItem tabItem = new TabItem();
+            tabItem.Content = grid;
+            tabItem.Header = name;
+            return tabItem;
+        }
+
+        public void CreateOutputTabPanel(uint tabPanel, string name)
+        {
+            if(tabs.Count > tabPanel)
+            {
+                tabs[(int)tabPanel].Items.Add(CreateTabItem(name));
+            }
+        }
+
+        public void CloseOutputTabPanel(uint tabPanel)
+        {
+            if (tabs.Count > tabPanel)
+            {
+                tabs[(int)tabPanel].Items.Clear();
+            }
+        }
+
+        public void CloseOutputTermminal(uint tabPanel, string name)
+        {
+            if (tabs.Count > tabPanel)
+            {
+                var tab = tabs[(int)tabPanel];
+                var item = tab.Items.Cast<TabItem>().First(n => n.Name == name);
+                tab.Items.Remove(item);
+            }
+        }
+
         public MainWindow()
         {
             InitializeComponent();
+            Init();
+            
+            CreateOutputTabPanel(1, "test");
+        }
+
+        private void Init()
+        {
+            tabs.Add(TabControl0);
+            tabs.Add(TabControl1);
+            tabs.Add(TabControl2);
         }
     }
 }
---
+++ WinIO/WinIOMain/IOMain.cs
@@ -0,0 +1,33 @@
+﻿using Python.Runtime;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WinIO
+{
+    class IOMain
+    {
+        static System.Windows.Application application = new System.Windows.Application();
+
+        [STAThread]
+        static void Main(string[] args)
+        {
+            using (Py.GIL())
+            {
+                dynamic sys = Py.Import("sys");
+                Console.WriteLine(sys.path);
+
+                application.Run(new MainWindow());               
+            }
+
+            //Thread worker = new Thread(new ThreadStart(LoadWpfApp));
+            //worker.SetApartmentState(ApartmentState.STA);
+            //worker.Name = "WpfThread";
+            //worker.IsBackground = true;
+            //worker.Start();
+        }
+    }
+}
