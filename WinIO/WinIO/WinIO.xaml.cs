113fb8f729c8737cde58fc9c29b43d09d49028ca
sak9188 <sak9188@163.com>
2020-12-14 21:09:39 +0800

    完成了一个exe的整合

--- WinIO/WinIO/WinIO.xaml.cs
+++ WinIO/WinIO/WinIO.xaml.cs
@@ -6,12 +6,12 @@ using System.Linq;
 using System.Threading.Tasks;
 using System.Windows;
 
-namespace WinIO
+namespace WinIO.WPF
 {
     /// <summary>
     /// App.xaml 的交互逻辑
     /// </summary>
-    public partial class App : Application
+    public partial class WinIOApp : Application
     {
     }
 }
--- WinIO/WinIOMain/IOMain.cs
+++ WinIO/WinIOMain/IOMain.cs
@@ -1,26 +1,49 @@
 ﻿using Python.Runtime;
 using System;
 using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
 using System.Linq;
 using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
+using WinIO.WPF;
 
-namespace WinIO
+namespace WinIO.IOConsole
 {
     class IOMain
     {
-        static System.Windows.Application application = new System.Windows.Application();
+        // static WinIOApp application = null;
+
+        private static Assembly OnResolveAssembly(object sender, ResolveEventArgs args)
+        {
+            string resourceName = new AssemblyName(args.Name).Name;
+            resourceName = Assembly.GetExecutingAssembly().GetManifestResourceNames().First(x => x.Contains(resourceName+".dll"));
+            using (var stream = Assembly.GetExecutingAssembly().GetManifestResourceStream(resourceName))
+            {
+                byte[] assemblyData = new byte[stream.Length];
+                stream.Read(assemblyData, 0, assemblyData.Length);
+                return Assembly.Load(assemblyData);
+            }
+        }
 
         [STAThread]
         static void Main(string[] args)
         {
+            AppDomain.CurrentDomain.AssemblyResolve += OnResolveAssembly;
+            RealMain();
+        }
+
+        static void RealMain()
+        {
+            WinIOApp application = new WinIOApp();
+
             using (Py.GIL())
             {
                 dynamic sys = Py.Import("sys");
                 Console.WriteLine(sys.path);
 
-                application.Run(new MainWindow());               
+                application.Run(new MainWindow());
             }
 
             //Thread worker = new Thread(new ThreadStart(LoadWpfApp));
