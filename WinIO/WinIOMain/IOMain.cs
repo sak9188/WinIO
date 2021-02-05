6d7e643d5cdd90d9aacde88b7ae53792178450fd
HK <sak9188@163.com>
2021-02-05 11:30:02 +0800

    增加输出端的log

--- WinIO/WinIOMain/IOMain.cs
+++ WinIO/WinIOMain/IOMain.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Reflection;
 using System.Resources;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading;
 using System.Threading.Tasks;
 using System.Windows;
@@ -49,9 +50,33 @@ namespace WinIO.IOConsole
                 WinIOAPP.CustomInit();
                 PythonEngine.EndAllowThreads(state);
             }
-            catch (Exception)
+            catch (Exception e)
             {
-                throw;
+                //获取项目的绝对路径
+                using (FileStream fs = new FileStream("./crash.log", FileMode.Append))
+                {
+                    using (StreamWriter sw = new StreamWriter(fs))
+                    {
+                        sw.WriteLine(e.Message);
+                        if(e.Source.StartsWith("Python"))
+                        {
+                            string stack = e.StackTrace;
+                            int idx = stack.IndexOf(']');
+                            stack = stack.Substring(0, idx);
+                            string pattern = "(')(?:(?!\\1).)*?\\1";
+                            sw.Write(e.Message + "\n");   //将内容写入文件
+                            sw.WriteLine("Traceback");
+                            foreach (Match match in Regex.Matches(stack, pattern))
+                            {
+                                var tims = match.Value.Trim('\'');
+                                tims = tims.Replace("\\n", "\n");
+                                sw.Write(tims);
+                            }
+                        }
+                        sw.WriteLine();
+                    }
+                }
+                Environment.Exit(1);
             }
             while (true)
             {
