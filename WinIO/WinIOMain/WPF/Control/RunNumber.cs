e03ac3827a6c13ec0fcf661cff0f815225cf02bc
HK <sak9188@163.com>
2021-02-05 10:00:47 +0800

    修复一些bug

---
+++ WinIO/WinIOMain/WPF/Control/RunNumber.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Documents;
+using System.Windows.Media;
+
+namespace WinIO.WPF.Control
+{
+    public class RunNumber : Run
+    {
+        public int number;
+        public int Number
+        {
+            get
+            {
+                return number;
+            }
+            set
+            {
+                number = value;
+                Text = number.ToString() + "次>> ";
+            }
+        }
+
+        public RunNumber(int num)
+        {
+            this.Foreground = new SolidColorBrush(Colors.YellowGreen);
+            Number = num;
+        }
+
+    }
+}
